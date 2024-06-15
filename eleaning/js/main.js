08800a0cf21ad3d78a37ccee0af1a25aacef8151
tuannguyen20198 <lntt20198@gmail.com>
2024-06-15 09:31:00 +0700

    done courses

---
+++ eleaning/js/main.js
@@ -0,0 +1,19 @@
+window.addEventListener("load",function() {
+    truncateCardTitle();
+})
+function truncateCardTitle(){
+ var cardList = document.getElementsByClassName("card-title");
+ console.log(cardList);
+ for (var i =0; i < cardList.length;i++) {
+    var text = cardList[i].innerHTML;
+    var newText = trancateString(text,40);
+    cardList[i].innerHTML = newText;
+ }
+}
+function trancateString(str,num) {
+    if (str.length > num) {
+        return str.slice(0,num) + "...";
+    }else{
+        return str;
+    }
+}

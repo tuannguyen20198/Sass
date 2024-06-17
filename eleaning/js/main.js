8cdfef26e9637a949d2da8e406e8ba65af32252f
lnttuan <lnttuan@vnpt.vn>
2024-06-17 22:28:24 +0700

    code students

--- eleaning/js/main.js
+++ eleaning/js/main.js
@@ -1,16 +1,18 @@
 window.addEventListener("load", function () {
   truncateCardTitle();
 });
+
 function truncateCardTitle() {
   var cardList = document.getElementsByClassName("card-title");
   console.log(cardList);
   for (var i = 0; i < cardList.length; i++) {
     var text = cardList[i].innerHTML;
-    var newText = trancateString(text, 40);
+    var newText = truncateString(text, 35);
     cardList[i].innerHTML = newText;
   }
 }
-function trancateString(str, num) {
+
+function truncateString(str, num) {
   if (str.length > num) {
     return str.slice(0, num) + "...";
   } else {
